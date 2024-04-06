7cba7713fe6e9728df98c64cd67bbfdc3273ec8e
Amkakira <164623285+Amkakira@users.noreply.github.com>
2024-04-06 02:20:57 +0200

    Update kana.js

--- plugins/kana.js
+++ plugins/kana.js
@@ -5,7 +5,7 @@ handler.all = async function(m) {
   const chat = global.db.data.chats[m.chat];
   if (/^لوفي$/i.test(m.text) && !chat.isBanned) {
     conn.sendPresenceUpdate('recording', m.chat);
-    await m.reply(`*اهلا, ¿انا لوفي?*`);
+    await m.reply(`*اخو اكيرا الروح بلروح*`);
     m.conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
   }
   return !0;
