213cdf489c520ee09d9d448db95fe59cf4e7a828
Amkakira <164623285+Amkakira@users.noreply.github.com>
2024-04-08 02:57:47 +0200

    Update رياضه2.js

--- plugins/رياضه2.js
+++ plugins/رياضه2.js
@@ -11,13 +11,13 @@ handler.before = async function (m) {
             global.db.data.users[m.sender].exp += math.bonus
             clearTimeout(this.math[id][3])
             delete this.math[id]
-            m.reply(`✅ *الإجابة الصحيحة!*\n\n‣ فاز : *+${math.bonus} XP*`)
+            m.reply(`┃✅ ❯ *الإجابة الصحيحة!*\n\n‣ فاز : *+${math.bonus} XP*`)
         } else {
             if (--this.math[id][2] == 0) {
                 clearTimeout(this.math[id][3])
                 delete this.math[id]
                 m.reply(`*لقد انتهت المحاولات*\n\n إجابة : *${math.result}*`)
-      } else m.reply(`❎ *إجابة خاطئة*\n\nلا تزال هناك  ${this.math[id][2]} محاولات`)
+      } else m.reply(`┃❎ ❯ *إجابة خاطئة*\n\nلا تزال هناك  ${this.math[id][2]} محاولات`)
         }
     }
     return !0
