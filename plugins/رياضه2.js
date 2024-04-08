c96aa8c6ad41141ef78a30475682a102d1fd3d44
Amkakira <164623285+Amkakira@users.noreply.github.com>
2024-04-08 05:19:53 +0200

    Update رياضه2.js

--- plugins/رياضه2.js
+++ plugins/رياضه2.js
@@ -4,14 +4,14 @@ handler.before = async function (m) {
     let id = m.chat
     if (!m.quoted || !m.quoted.fromMe || !m.text || !/^احسب/i.test(m.quoted.text)) return !0
     this.math = this.math ? this.math : {}
-    if (!(id in this.math)) return this.reply(m.chat, 'انتهت اللعبة', m)
+    if (!(id in this.math)) return this.reply(m.chat, '❖┃  انتهت العبه ┃❌ ❯*', m)
     if (m.quoted.id == this.math[id][0].id) {
         let math = JSON.parse(JSON.stringify(this.math[id][1]))
         if (m.text == math.result) {
             global.db.data.users[m.sender].exp += math.bonus
             clearTimeout(this.math[id][3])
             delete this.math[id]
-            m.reply(`┃✅ ❯ *الإجابة الصحيحة!*\n\n‣ فاز : *+${math.bonus} XP*`)
+            m.reply(`┃✅ ❯ *الإجابة صحيحة!*\n\n‣ *فاز* : *+${math.bonus} XP*`)
         } else {
             if (--this.math[id][2] == 0) {
                 clearTimeout(this.math[id][3])
