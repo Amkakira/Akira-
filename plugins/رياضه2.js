97009af39db7df21e0ede0164ed54d489b6d35e7
Amkakira <164623285+Amkakira@users.noreply.github.com>
2024-04-08 05:20:58 +0200

    Update رياضه2.js

--- plugins/رياضه2.js
+++ plugins/رياضه2.js
@@ -2,7 +2,7 @@ let handler = m => m
 handler.before = async function (m) {
     if (!/^-?[0-9]+(\.[0-9]+)?$/.test(m.text)) return !0
     let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.text || !/^احسب/i.test(m.quoted.text)) return !0
+    if (!m.quoted || !m.quoted.fromMe || !m.text || !/^*احسب/i.test(m.quoted.text)) return !0
     this.math = this.math ? this.math : {}
     if (!(id in this.math)) return this.reply(m.chat, '❖┃  انتهت العبه ┃❌ ❯*', m)
     if (m.quoted.id == this.math[id][0].id) {
