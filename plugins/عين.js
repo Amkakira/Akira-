e3e3ab0221d6e47b00b492abd0e5481ed11e09fa
Amkakira <164623285+Amkakira@users.noreply.github.com>
2024-04-09 14:33:24 +0200

    Update عين.js

--- plugins/عين.js
+++ plugins/عين.js
@@ -1,31 +1,31 @@
-let timeout = 40000
-let poin = 5000
+let let timeout = 60000
+let poin = 500
 let handler = async (m, { conn, command, usedPrefix }) => {
-    conn.tokitoki = conn.tokitoki ? conn.tokitoki : {}
+    conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
     let id = m.chat
-    if (id in conn.tokitoki) {
-        conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.tokitoki[id][0])
+    if (id in conn.tebakbendera) {
+        conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.tebakbendera[id][0])
         throw false
     }
-    let src = await (await fetch('https://raw.githubusercontent.com/kimos71/AURTER-BOT/main/Games/eyeanime.json')).json()
+    let src = await (await fetch('https://raw.githubusercontent.com/Afghhjjkoo/GURU-BOT/main/lib/mark.json')).json()
   let json = src[Math.floor(Math.random() * src.length)]
-    let caption = `*❰❖── ~『𝑁𝐸𝑍𝑈𝐾𝛩–𝐵𝛩𝑇』~──❖❱*\n *•┇❖↞استخدم انسحب للانسحاب┇👁️❯*
- *•┃❖↞الـوقـت⏳↞* *${(timeout / 1000).toFixed(2)}* *ثانية┇❯*
-  
- *•┃❖↞الـجـائـزة💰↞* *${poin}* *نقطه┇❯*
-   *❰❖── ~『𝑁𝐸𝑍𝑈𝐾𝛩–𝐵𝛩𝑇』~──❖❱*
+    let caption = `*${command.toUpperCase()}*
+  ❐↞┇الـوقـت⏳↞ *${(timeout / 1000).toFixed(2)} ┇
+  *•┇❖↞استخدم انسحب للانسحاب┇👁️❯*
+  ❐↞┇الـجـائـزة💰↞ ${poin} نقاط┇
+*❖━━━━━━[ 𝑁𝐸𝑍𝑈𝐾𝛩-𝐵𝛩𝑇 ]━━━━━❖*
      `.trim()
-    conn.tokitoki[id] = [
+    conn.tebakbendera[id] = [
         await conn.sendFile(m.chat, json.img, '', caption, m),
         json, poin,
         setTimeout(() => {
-            if (conn.tokitoki[id]) conn.reply(m.chat, `*❮ ⌛┇انتــهــى الــوقــت┇⌛❯*\n*❖↞┇الاجـابـة✅↞*  *${json.name}* *┇❯*`, conn.tokitoki[id][0])
-            delete conn.tokitoki[id]
+            if (conn.tebakbendera[id]) conn.reply(m.chat, `*❮ ⌛┇انتــهــى الــوقــت┇⌛❯*\n❐↞┇الاجـابـة✅↞ ${json.name}*┇`, conn.tebakbendera[id][0])
+            delete conn.tebakbendera[id]
         }, timeout)
     ]
 }
-handler.help = ['guesseye']
+handler.help = ['guessflag']
 handler.tags = ['game']
-handler.command = /^عين$/i
+handler.command = /^عين/i
 
 export default handler
