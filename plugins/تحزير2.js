3ad02c3452a32acf60fe12a3d704fb98855fd70c
Amkakira <164623285+Amkakira@users.noreply.github.com>
2024-04-11 13:55:23 +0200

    Update تحزير2.js

--- plugins/تحزير2.js
+++ plugins/تحزير2.js
@@ -1,19 +1,27 @@
-const handler = async (m, {conn, text, command, usedPrefix}) => {
-  const pp = './src/warn.jpg';
-  let who;
-  if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text;
-  else who = m.chat;
-  const user = global.db.data.users[who];
-  const bot = global.db.data.settings[conn.user.jid] || {};
-  const warntext = `*[❗] ضع علامة على شخص ما أو قم بالرد على رسالة جماعية*\n\n*مثال:*\n*${usedPrefix + command} @${global.suittag}*`;
-  if (!who) throw m.reply(warntext, m.chat, {mentions: conn.parseMention(warntext)});
-  if (m.mentionedJid.includes(conn.user.jid)) return;
-  if (user.warn == 0) throw '*[❗] المستخدم لديه 0 تحذيرات*';
-  user.warn -= 1;
-  await m.reply(`${user.warn == 1 ? `*@${who.split`@`[0]}*` : `♻️ *@${who.split`@`[0]}*`} تمت إزالة التحذير\n*تحذيرات ${user.warn}/3*`, null, {mentions: [who]});
-};
-handler.command = /^(شيل-التحذير|delwarn|deladvertir|deladvertencia|delwarning)$/i;
-handler.group = true;
-handler.admin = true;
-handler.botAdmin = true;
-export default handler;
+let handler = async (m, { conn, text, command, usedPrefix }) => {
+let pp = './src/warn.jpg'
+let who
+if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
+else who = m.chat
+let user = global.db.data.users[who]
+let bot = global.db.data.settings[conn.user.jid] || {}
+let warntext = `*[❗] يرجي وضع منشن للشخص او الرد علي رساله له @منشن*\n\n*—◉ مثال:*\n*${usedPrefix + command} @${global.suittag}*`
+if (!who) throw m.reply(warntext, m.chat, { mentions: conn.parseMention(warntext)}) 
+user.warn += 1
+  
+await conn.sendButton(m.chat,`${user.warn == 1 ? `*@${who.split`@`[0]}*` : `*@${who.split`@`[0]}*`} 𝚁حصلت على تحذير في هذه المجموعة!`, `*تحذيرات ${user.warn}/3*\n\n${wm}`, pp, [['📋 قائمة التحذيرات 📋', '#listwarn']], m, { mentions: [who] })
+	
+if (user.warn >= 3) {
+if (!bot.restrict) return m.reply('*[❗] لم يتم تمكين القيود على مالك الروبوت (#تفعيل) استخدم هذا الامر لتمكين التحذيرات*')        
+user.warn = 0
+await m.reply(`لقد حذرتك عدة مرات!!\n*@${who.split`@`[0]}* حتي اصبحوا *3* تحذيرات سوف يتم طردك بعد 0 ثانية 👽`, null, { mentions: [who]})
+//user.banned = true
+await conn.groupParticipantsUpdate(m.chat, [who], 'remove') 
+} 
+return !1
+}
+handler.command = /^(انقلع|انذار|warn|تحذير)$/i
+handler.group = true
+handler.owner = true
+handler.owner = true
+export default handler
