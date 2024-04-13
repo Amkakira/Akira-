478545c3e02a46451e1ad5bf1784eea1cb59b51b
Amkakira <164623285+Amkakira@users.noreply.github.com>
2024-04-13 15:48:22 +0200

    Update كوره.js

--- plugins/كوره.js
+++ plugins/كوره.js
@@ -16,7 +16,6 @@ let handler = async (m, { conn, usedPrefix }) => {
     let clue = _clue.replace(/[A-Za-z]/g, ''); // Fixed this line
     let caption = `
 ⷮ ${json.question}
-
 ❐↞┇الـوقـت⏳↞ ${(timeout / 1000).toFixed(2)}┇
 ❐↞┇الـجـائـزة💰↞ ${poin} نقاط┇
 *❖━━━━━━[ 𝑁𝐸𝑍𝑈𝐾𝛩-𝐵𝛩𝑇 ]━━━━━❖*
