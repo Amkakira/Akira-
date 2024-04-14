44908883ec9519135a793c0ac97a7ab4870cc23d
Amkakira <164623285+Amkakira@users.noreply.github.com>
2024-04-15 01:43:47 +0200

    Update مخفي.js

--- plugins/مخفي.js
+++ plugins/مخفي.js
@@ -1,9 +1,9 @@
-let handler = async (m, { conn, text, participants }) => {
+let handler = async (m, { conn, participants }) => {
     let users = participants.map(u => conn.user.jid)
     let q = m.quoted ? m.quoted : m
-    let msg = await conn.sendMessage(m.chat, text || '', 'extendedTextMessage', { quoted: q })
+    let msg = await conn.sendMessage(m.chat, '', 'extendedTextMessage', { quoted: q })
     let mentionedJid = users.filter(user => user !== conn.user.jid)
-    await conn.sendMessage(msg.key.remoteJid, text || '', 'extendedTextMessage', { contextInfo: { mentionedJid }, quoted: msg })
+    await conn.sendMessage(msg.key.remoteJid, '', 'extendedTextMessage', { contextInfo: { mentionedJid }, quoted: msg })
 }
 handler.help = ['hidetag']
 handler.tags = ['group']
