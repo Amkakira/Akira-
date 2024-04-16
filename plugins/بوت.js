4a1df5b32a1f71f885b5d92b89aae13470955037
Amkakira <164623285+Amkakira@users.noreply.github.com>
2024-04-16 20:07:58 +0200

    Update بوت.js

--- plugins/بوت.js
+++ plugins/بوت.js
@@ -19,6 +19,7 @@ let handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPr
     let uptime = clockString(_uptime)
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
+    await conn.sendMessage(m.chat, { react: { text: '🌋', key: m.key } })
 let videoUrl = 'https://telegra.ph/file/bf30d73e832b4c2416b32.mp4';
   let vn = './media/menu.mp3';
   const user = global.db.data.users[m.sender];
