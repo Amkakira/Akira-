18faf3ee35e9b9743680978a0af8c14618737829
Amkakira <164623285+Amkakira@users.noreply.github.com>
2024-04-16 14:54:41 +0200

    Update ترحيب و خروج.js

--- plugins/ترحيب و خروج.js
+++ plugins/ترحيب و خروج.js
@@ -29,6 +29,24 @@ let handler3 = async (m, { conn, participants }) => {
     }
 };
 
+let handler4 = async (m, { conn, participants }) => {
+    let user = participants.find(user => user.jid === m.sender);
+    if (user) {
+        let newUser = m.action.split(" ")[0].replace('@', '').replace('c.us', '');
+        let desc = await conn.groupMetadata(m.chat).catch(() => {});
+        let welcomeMessageWithDesc = `${welcomeMessage}\n\nوصف الجروب:\n${desc.desc}\n\n@${newUser}`;
+        await conn.reply(m.chat, `تمت إضافة عضو جديد إلى الجروب.\n${welcomeMessageWithDesc}`, m);
+    }
+};
+
+let handler5 = async (m, { conn, participants }) => {
+    let user = participants.find(user => user.jid === m.sender);
+    if (user) {
+        let target = m.action.split(" ")[0].replace('@', '').replace('c.us', '');
+        await conn.sendMessage(target + '@c.us', `تمت طرد العضو من الجروب. رقم العضو: ${m.action.split(" ")[0].replace('@', '').replace('c.us', '')}`);
+    }
+};
+
 handler.group = true;
 handler.event = ["groupJoin"];
 handler.command = /^$/i;
@@ -41,8 +59,18 @@ handler3.group = true;
 handler3.event = ["groupJoin"];
 handler3.command = /^$/i;
 
+handler4.group = true;
+handler4.event = ["groupAdd"];
+handler4.command = /^$/i;
+
+handler5.group = true;
+handler5.event = ["groupRemove"];
+handler5.command = /^$/i;
+
 module.exports = {
     handler,
     handler2,
-    handler3
+    handler3,
+    handler4,
+    handler5
 };
